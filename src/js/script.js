b2577047e3c6d3ad41b9b5a1ac10910366eb381e
Юрий Бондарчук <nikitaodessafreelance@gmail.com>
2020-06-19 18:11:16 +0300

    add tabs and fix preloader

--- src/js/script.js
+++ src/js/script.js
@@ -452,8 +452,17 @@ $(".menu-element").on('click', function(e) {
     );
 
 
-
+// tabs
+$('ul.portfolio__tabs').on('click', 'li:not(.portfolio__tab_active)', function() {
+  $(this)
+    .addClass('portfolio__tab_active').siblings().removeClass('portfolio__tab_active')
+    .closest('div._container').find('div.portfolio__content').removeClass('portfolio__content_active').eq($(this).index()).addClass('portfolio__content_active');
+});
    
-
+$('.portfolio__item').on('click', function() {
+  $(this)
+  .addClass('portfolio__item_active').siblings().removeClass('portfolio__item_active');
+});
+// tabs end
 
 });
