d12e233c98e30c46a0c0948d2ce770e229a90c4c
Nikita <nikitaodessafreelance@gmail.com>
2020-08-09 18:12:03 +0300

    add background on main block

--- dist/js/script.js
+++ dist/js/script.js
@@ -873,5 +873,18 @@ $('#scrollTop').on('click',(e) => {
 }) 
 //  scroll top end
 
+// ibg
+function ibg(){
+
+  let ibg=document.querySelectorAll("._ibg");
+  for (var i = 0; i < ibg.length; i++) {
+  if(ibg[i].querySelector('img')){
+  ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+  }
+  }
+  }
+  
+  ibg();
+  // ibg end
 
 });
